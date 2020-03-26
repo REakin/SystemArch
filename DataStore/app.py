167c8f0da6a5f38c4cb276dafec15d8543dbc7c0
Ryan Eakin <Eakinryan@hotmail.com>
2020-03-26 11:39:13 -0700

    fix

--- DataStore/app.py
+++ DataStore/app.py
@@ -38,7 +38,7 @@ logger = logging.getLogger('basicLogger')
 
 
 #db setup
-DB_ENGINE = create_engine(f'mysql+pymysql://{app_config["datastore"]["user"]}:{app_config["datastore"]["password"]}@localhost:{app_config["datastore"]["port"]}/{app_config["datastore"]["db"]}')
+DB_ENGINE = create_engine(f'mysql+pymysql://{app_config["datastore"]["user"]}:{app_config["datastore"]["password"]}@{app_config["datastore"]["hostname"]}:{app_config["datastore"]["port"]}/{app_config["datastore"]["db"]}')
 Base.metadata.bind = DB_ENGINE
 DB_SESSION = sessionmaker(bind=DB_ENGINE)
 
